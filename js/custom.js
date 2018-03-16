adf4813c22f99c1dedcd146305228cce650d1d2a
Dinesh Vaghasia <vaghasiadinesh@gmail.com>
2018-03-16 23:02:50 +0530

    initial comit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+
+$(function() {
+	
+	/* Top Search button */
+	$('.fa-search').click(function(){
+		$(this).addClass('yellow');
+		$('.searchbox').fadeIn();
+	});
+	
+});
